4f2647d2c511e14ee423698b97fa5cf69b6b5e52
DevilMayCry2333 <1712363499@qq.com>
2019-10-04 10:45:34 +0800

    最新

--- src/main/java/cn/huihongcloud/controller/OtherBeetleController.java
+++ src/main/java/cn/huihongcloud/controller/OtherBeetleController.java
@@ -1,14 +1,18 @@
 package cn.huihongcloud.controller;
 
+import cn.huihongcloud.entity.*;
 import cn.huihongcloud.entity.beetle.BeetleInfo;
 import cn.huihongcloud.entity.common.Result;
 import cn.huihongcloud.entity.user.User;
+import cn.huihongcloud.mapper.OtherBeetleMapper;
 import cn.huihongcloud.service.OtherBeetleService;
 import cn.huihongcloud.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by 钟晖宏 on 2019/1/16
@@ -22,6 +26,168 @@ public class OtherBeetleController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private OtherBeetleMapper otherBeetleMapper;
+
+    //a5fdlQfLzKbQPLpdLEDczQ==
+
+    @GetMapping("/deadTrees_Kill")
+    public Object e() {
+        return Result.ok(otherBeetleMapper.getDead_Kill());
+    }
+
+    @PostMapping("/deadTrees_Kill")
+    public Object f(String name) {
+        otherBeetleMapper.insertDead_Kill(name);
+        return Result.ok();
+    }
+
+
+    @PutMapping("/deadTrees_Kill")
+    public Object g(deadTree_KillMethods deadTreeKillMethods) {
+        otherBeetleMapper.updateDead_Kill(deadTreeKillMethods);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/deadTrees_Kill")
+    public Object h(int id) {
+        otherBeetleMapper.deleteDead_Kill(id);
+        return Result.ok();
+    }
+
+
+    @GetMapping("/enemy_Type")
+    public Object a() {
+        return Result.ok(otherBeetleMapper.getEnemy_Type());
+    }
+
+    @PostMapping("/enemy_Type")
+    public Object b(String name) {
+        otherBeetleMapper.insertEnemy_Type(name);
+        return Result.ok();
+    }
+
+
+    @PutMapping("/enemy_Type")
+    public Object c(enemy_EneType enemyEneType) {
+        otherBeetleMapper.updateEnemy_Type(enemyEneType);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/enemy_Type")
+    public Object d(int id) {
+        otherBeetleMapper.deleteEnemy_Type(id);
+        return Result.ok();
+    }
+
+
+    @GetMapping("/inject_workContent")
+    public Object getinjectWork() {
+        return Result.ok(otherBeetleMapper.getInject_WorkContent());
+    }
+
+    @PostMapping("/inject_workContent")
+    public Object addInjecta(String name) {
+        otherBeetleMapper.insertInject_WorkContent(name);
+        return Result.ok();
+    }
+
+
+    @PutMapping("/inject_workContent")
+    public Object editInjecta(inject_WorkContent injectWorkContent) {
+        otherBeetleMapper.updateInject_WorkContent(injectWorkContent);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/inject_workContent")
+    public Object deleteInjeca(int id) {
+        otherBeetleMapper.deleteWorkContent(id);
+        return Result.ok();
+    }
+
+
+    @GetMapping("/inject_woodstatus")
+    public Object getinjectWood() {
+        return Result.ok(otherBeetleMapper.getInject_WoodStatus());
+    }
+
+    @PostMapping("/inject_woodstatus")
+    public Object addInjectWood(String name) {
+        otherBeetleMapper.insertInject_WoodStatus(name);
+        return Result.ok();
+    }
+
+
+    @PutMapping("/inject_woodstatus")
+    public Object editInjectWood(inject_WoodStatus woodStatus) {
+        otherBeetleMapper.updateInject_WoodStatus(woodStatus);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/inject_woodstatus")
+    public Object deleteInjectWood(int id) {
+        otherBeetleMapper.deleteInject_WoodStatus(id);
+        return Result.ok();
+    }
+
+
+    @GetMapping("/inject")
+    public Object getinject() {
+        return Result.ok(otherBeetleMapper.getInjectInfo());
+    }
+
+    @PostMapping("/inject")
+    public Object addInjectInfo(String name) {
+        otherBeetleMapper.insertInjectInfo(name);
+        return Result.ok();
+    }
+
+
+    @PutMapping("/inject")
+    public Object editInjectInfo(injectInfo injectInfo) {
+        otherBeetleMapper.updateInjectInfo(injectInfo);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/inject")
+    public Object deleteInjectInfo(int id) {
+        otherBeetleMapper.deleteInjectInfo(id);
+        return Result.ok();
+    }
+
+
+    @GetMapping("/workContent")
+    public Object getWorkContent() {
+        return Result.ok(otherBeetleService.getWorkContentList());
+    }
+
+    @PostMapping("/workContent")
+    public Object addworkContentInfo(String name) {
+        List<workContent> workContentList = otherBeetleService.getWorkContentList();
+        int maxFvalue=0;
+        for (workContent workContent:workContentList) {
+            if(workContent.getFvalue()>maxFvalue)
+                maxFvalue = workContent.getFvalue();
+        }
+        maxFvalue = maxFvalue + 1;
+        otherBeetleMapper.insertWorkContent(name,maxFvalue);
+        return Result.ok();
+    }
+
+
+    @ApiOperation("")
+    @PutMapping("/workContent")
+    public Object editworkContentInfo(workContent workcontent) {
+        otherBeetleMapper.updateWorkContent(workcontent);
+        return Result.ok();
+    }
+
+    @DeleteMapping("/workContent")
+    public Object deleteWorkContentInfo(int id) {
+        otherBeetleMapper.deleteWorkContent(id);
+        return Result.ok();
+    }
 
     // 这个就不分页了
     @ApiOperation("查看其他天牛列表")
