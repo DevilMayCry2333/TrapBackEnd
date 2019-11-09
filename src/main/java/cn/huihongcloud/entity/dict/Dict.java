f5831147e7a165e957a3dd8941c8ffbee77249c0
ykaiyu <1712363499@qq.com>
2019-11-09 12:56:29 +0800

    提交

--- src/main/java/cn/huihongcloud/entity/dict/Dict.java
+++ src/main/java/cn/huihongcloud/entity/dict/Dict.java
@@ -19,4 +19,27 @@ public class Dict {
     @TableField("`value`")
     private String value;
 
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
