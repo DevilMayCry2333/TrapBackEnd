8ed2013557b7f3a5c1b0ae5618b7fac8cf722757
ykaiyu <1712363499@qq.com>
2019-12-02 21:20:48 +0800

    nmsl

--- src/main/java/cn/huihongcloud/controller/newApp/MyTrack.java
+++ src/main/java/cn/huihongcloud/controller/newApp/MyTrack.java
@@ -193,7 +193,7 @@ public class MyTrack {
             logger.info("无图片轨迹");
             jsonObject.put("isComp",true);
             device_track_maintanceEntity.setPic1("pic1");
-            if(lineName!=null && !lineName.equals(""))
+//            if(lineName!=null && !lineName.equals(""))
                 deviceTrackMaintanceEntityMapper.addMaintance(device_track_maintanceEntity);
         }
         return jsonObject;
